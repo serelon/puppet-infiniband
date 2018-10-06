05540081ac23a8737fa592dc14222ac25da75a13
Therese Säwenmyr <sawenmyr@gmail.com>
2018-10-06 13:04:27 +0200

    removed ofed-docs

--- manifests/params.pp
+++ manifests/params.pp
@@ -56,7 +56,6 @@ class infiniband::params {
                                     'librdmacm-dev',
                                     'librdmacm1',
                                     'rdmacm-utils',
-                                    'ofed-docs',
                                     'libopensm5a',
                                     ],
         default => []
